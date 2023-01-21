30e5ee68b2b0b2f53c78513d93cbe2f8467ccba1
Kris <stawinogakris@gmail.com>
2023-01-21 15:45:17 +0100

    feat: camera movement (thx winapi)

--- MGK2/main.cpp
+++ MGK2/main.cpp
@@ -1,47 +1,74 @@
-﻿#include "HVector.h"
-#include "HMatrix4.h"
-#include <iostream>
+﻿#include <iostream>
+#include <windows.h>
 
-#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006 
-
-#define PI_BUT_FUNNY 22/7 
-
-#include "HCube.h"
-#include "HLine.h"
-#include "HPlane.h"
 #include "HQuat.h"
 #include "HRTX.h"
-#include "HSegment.h"
-#include "HSphere.h"
 #include "HUtils.h"
-#include "HVector4.h"
+#include "HVector.h"
 
-using namespace std;
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006 
+#define PI_BUT_FUNNY (22/7)
 
-int main()
+void redraw(float pitch, float yaw, float radius)
 {
-	//HCube Cube = HCube(1);
-
 	HRTX raytracer;
-
-	float pitch = 0.33f;
-	float yaw = -0.22f;
-	float radius = 50.0f;
 
 	HVector camPos(1, 1, 1);
 	HVector camDir = camPos * -1.0f;
 
 	const float pitchRadians = pitch * PI / 180.0f;
 	const float yawRadians = yaw * PI / 180.0f;
+
 	camPos.x = radius * std::cos(pitchRadians) * std::cos(yawRadians);
 	camPos.y = radius * std::sin(pitchRadians) * std::cos(yawRadians);
 	camPos.z = radius * std::sin(yawRadians);
-	//std::system("cls");
+
 	camDir.Normalize();
+
+	std::system("cls");
 	raytracer.RayCast(camPos, camDir);
 	raytracer.Draw();
+}
 
-	std::cout << "dupa debugging: " << camPos.ToString() << "\n" << camDir.ToString() << "\n";
+int main()
+{
+	float pitch = 0.33f;
+	float yaw = -0.22f;
+	constexpr float radius = 50.0f;
+
+	redraw(pitch, yaw, radius);
+
+	// WINAPIIIIIIIIIIiiiiiiiiiii-
+	HANDLE hstdin;
+	DWORD mode;
+
+	while(true)
+	{
+		hstdin = GetStdHandle(STD_INPUT_HANDLE);
+		GetConsoleMode(hstdin, &mode);
+		SetConsoleMode(hstdin, ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
+		const int opt = std::cin.get();
+		if (opt == 0x61) //a
+		{
+			pitch += 0.01f;
+			redraw(pitch, yaw, radius);
+		}
+		if (opt == 0x64) //d
+		{
+			pitch -= 0.01f;
+			redraw(pitch, yaw, radius);
+		}
+		if (opt == 0x77) //w
+		{
+			yaw += 0.01f;
+			redraw(pitch, yaw, radius);
+		}
+		if (opt == 0x73) //s
+		{
+			yaw -= 0.01f;
+			redraw(pitch, yaw, radius);
+		}
+	}
 
 	/*
 	// Sprawdzamy przemiennosc dodawania wektorow
